1abc5298051f1e97fceb98b4bfeda96d39a91efe
Alexandr <34417026+KamenkoAlexandr@users.noreply.github.com>
2018-02-20 15:49:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    var k=0;
+    for ( var i = 0; i < preferences.length; i++) {
+        if (preferences[i] - 1 == preferences[preferences[i] - 1] - 1) continue;
+        if (i == preferences[preferences[preferences[i] - 1] - 1] - 1) k++;
+    }
+    if(k < 3)
+        return 0;
+    return k/3;
 };
